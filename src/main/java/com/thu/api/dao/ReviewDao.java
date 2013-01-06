4af4bc0d0344429bec1d398ea0fc36d5259803f8
orderingqa <orderingqa@gmail.com>
2013-01-06 18:04:48 +0800

    delete all the review related files and keep code clean for first round review

--- src/main/java/com/thu/api/dao/ReviewDao.java
+++
@@ -1,7 +0,0 @@
-package com.thu.api.dao;
-
-import com.thu.api.domain.Review;
-
-public interface ReviewDao extends GenericDao<Review, Long> {
-
-}
--- src/main/java/com/thu/api/domain/service/ReviewService.java
+++
@@ -1,7 +0,0 @@
-package com.thu.api.domain.service;
-
-import com.thu.api.domain.Review;
-
-public interface ReviewService extends GenericDomainObjectService <Review, Long> {
-
-}
